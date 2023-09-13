81e7bdab1029964cb4b5d6f63b3c2d2b2c0d1b18
Sabrina <33787198+sreese147@users.noreply.github.com>
2023-09-13 15:29:38 -0400

    Update Corndog.java

--- Corndog.java
+++ Corndog.java
@@ -1,6 +1,24 @@
 package reese.teach;
 
 public class Corndog {
+    String toppings;
+    double price;
+    float hotdogCost;
+    Enum hotdogMeat;
+    boolean isKosher;
+    int calories;
+
+    private Corndog(){
+        this.toppings="None";
+        this.price = 3.99;
+        this.hotdogCost = 1.54f;
+        this.hotdogMeat= ;
+        this.isKosher = true;
+        this.calories =250;
+    }
+    private Corndog(String toppings, double price)
+
+
     /*
      * toppings string
      * price float 
@@ -9,4 +27,5 @@ public class Corndog {
      * is kosher bool
      * calories int
      */
+
 }
