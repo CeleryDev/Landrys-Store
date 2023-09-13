d9ff0ff8dd72f597eb6ff32229e0aa930059ce77
Sabrina <33787198+sreese147@users.noreply.github.com>
2023-09-13 15:14:29 -0400

    Add files via upload

---
+++ Corndog.java
@@ -0,0 +1,12 @@
+package reese.teach;
+
+public class Corndog {
+    /*
+     * toppings string
+     * price float 
+     * hotdog cost double
+     * hotdog meat enum
+     * is kosher bool
+     * calories int
+     */
+}
