f341e1787efb2bd198ff66cf5c6164b98c73c9a0
CeleryDev <66083859+CeleryDev@users.noreply.github.com>
2023-09-14 15:16:10 -0400

    Added some things

--- Corndog.java
+++ Corndog.java
@@ -49,6 +49,47 @@ public class Corndog {
         this.calories =calories;
     }
 
+    public String getToppings(){return this.toppings;}
+    public void setToppings(String toppings){this.toppings = toppings;}
+    
+    public double getPrice(){return this.price;}
+    public void setPrice(double price){this.price = price;}
+
+    public float getHotdogCost(){return this.hotdogCost;}
+    public void setHotdogCost(float hotdogCost){this.hotdogCost = hotdogCost;}
+
+    public Enum getHotdogMeat(){return this.hotdogMeat;}
+    public void setHotdogMeat(Enum hotdogMeat){this.hotdogMeat = hotdogMeat;}
+
+    public boolean getIsKosher(){return this.isKosher;}
+    public void setIsKosher(boolean isKosher){this.isKosher = isKosher;}
+
+    public int getCalories(){return this.calories;}
+    public void setCalories(int calories){this.calories = calories;}
+
+    public void displayCorndog() {
+        System.out.println("Corndog Report");
+        System.out.println("===========================");
+        System.out.println("Toppings: " + this.toppings);
+        System.out.println("Price: " + this.price);
+        System.out.println("Hotdog Cost: " + this.hotdogCost);
+        System.out.println("Hotdog Meat: " + this.hotdogMeat);
+        System.out.println("Is Kosher: " + this.isKosher);
+        System.out.println("Calories: " + this.calories);
+    }
+
+    public String toString() {
+        String s = "Corndog Report\n";
+        s += this.toppings + "\n";
+        s += this.price + "\n";
+        s += this.hotdogCost + "\n";
+        s += this.hotdogMeat + "\n";
+        s += this.isKosher + "\n";
+        s += this.calories + "\n";
+        return s;
+
+    }
+
 
 
 
