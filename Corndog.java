8584173ffc62254ddde78c9c1fc14639282dfd73
Sabrina <33787198+sreese147@users.noreply.github.com>
2023-09-14 14:54:32 -0400

    Update Corndog.java

--- Corndog.java
+++ Corndog.java
@@ -1,3 +1,11 @@
+/*Sabrina Reese + Ben
+ * Cmdr Schenk
+ * CSA Period 7
+ * 19 September 2023
+ * Corndog Class
+ */
+
+
 package reese.teach;
 
 public class Corndog {
@@ -16,16 +24,32 @@ public class Corndog {
         this.isKosher = true;
         this.calories =250;
     }
-    private Corndog(String toppings, double price)
+    private Corndog(String toppings, double price){
+        this.toppings= toppings;
+        this.price=price;
+        this.hotdogCost= 2.55f;
+        this.hotdogMeat= ;
+        this.isKosher = false;
+        this.calories =350;
+    }
+    private Corndog(double price, float hotdogCost, Enum hotdogMeat, boolean isKosher ){
+        this.toppings= "None";
+        this.price=price;
+        this.hotdogCost= hotdogCost;
+        this.hotdogMeat= hotdogMeat;
+        this.isKosher = isKosher;
+        this.calories =200;
+    }
+    private Corndog(String toppings, double price, float hotdogCost, Enum hotdogMeat, boolean isKosher, int calories ){
+        this.toppings= toppings;
+        this.price=price;
+        this.hotdogCost=hotdogCost;
+        this.hotdogMeat= hotdogMeat;
+        this.isKosher = isKosher;
+        this.calories =calories;
+    }
 
 
-    /*
-     * toppings string
-     * price float 
-     * hotdog cost double
-     * hotdog meat enum
-     * is kosher bool
-     * calories int
-     */
+
 
 }
