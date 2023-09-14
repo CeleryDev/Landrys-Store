a08b9f2a1019f9a761f70ed872b70993997a4a1a
Sabrina <33787198+sreese147@users.noreply.github.com>
2023-09-14 15:28:59 -0400

    Add files via upload

---
+++ hotdogMeat.java
@@ -0,0 +1,16 @@
+/*Sabrina Reese + Ben
+ * Cmdr Schenk
+ * CSA Period 7
+ * 19 September 2023
+ * Corndog ENum
+ */
+
+package reese.teach;
+
+public enum hotdogMeat {
+    Beef, 
+    Turkey, 
+    Pork, 
+    Vegan, 
+    Chicken
+}
