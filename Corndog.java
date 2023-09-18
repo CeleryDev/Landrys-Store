5b1e7d0b783ab61add341c4bf18ec59cc4b1e4d1
Sabrina <33787198+sreese147@users.noreply.github.com>
2023-09-18 14:41:36 -0400

    Update Corndog.java
    - he said dont do display j do 2string
    - totally forgot how constructors worked
    - also i do my getters+setters differently

--- Corndog.java
+++ Corndog.java
@@ -8,31 +8,36 @@
 
 package reese.teach;
 
+import reese.teach.hotdogMeat;
+
 public class Corndog {
     String toppings;
     double price;
     float hotdogCost;
-    Enum hotdogMeat;
+    hotdogMeat hotdogMeat;
     boolean isKosher;
     int calories;
 
-    private Corndog(){
+
+    
+    public Corndog(){
         this.toppings="None";
         this.price = 3.99;
         this.hotdogCost = 1.54f;
-        this.hotdogMeat= ;
+        this.hotdogMeat= hotdogMeat.Turkey;
         this.isKosher = true;
         this.calories =250;
     }
-    private Corndog(String toppings, double price){
+    public Corndog(String toppings, double price){
         this.toppings= toppings;
         this.price=price;
         this.hotdogCost= 2.55f;
-        this.hotdogMeat= ;
+        this.hotdogMeat= hotdogMeat.Pork;
         this.isKosher = false;
         this.calories =350;
     }
-    private Corndog(double price, float hotdogCost, Enum hotdogMeat, boolean isKosher ){
+    
+    public Corndog(double price, float hotdogCost, reese.teach.hotdogMeat hotdogMeat, boolean isKosher ){
         this.toppings= "None";
         this.price=price;
         this.hotdogCost= hotdogCost;
@@ -40,7 +45,7 @@ public class Corndog {
         this.isKosher = isKosher;
         this.calories =200;
     }
-    private Corndog(String toppings, double price, float hotdogCost, Enum hotdogMeat, boolean isKosher, int calories ){
+    public Corndog(String toppings, double price, float hotdogCost, reese.teach.hotdogMeat hotdogMeat, boolean isKosher, int calories ){
         this.toppings= toppings;
         this.price=price;
         this.hotdogCost=hotdogCost;
@@ -49,33 +54,52 @@ public class Corndog {
         this.calories =calories;
     }
 
-    public String getToppings(){return this.toppings;}
-    public void setToppings(String toppings){this.toppings = toppings;}
+    public String getToppings(){
+        return this.toppings;
+    }
+    public void setToppings(String toppings){
+        this.toppings = toppings;
+    }
     
-    public double getPrice(){return this.price;}
-    public void setPrice(double price){this.price = price;}
+    public double getPrice(){
+        return this.price;
+    }
+    
+    public void setPrice(double price){
+        this.price = price;
+    }
 
-    public float getHotdogCost(){return this.hotdogCost;}
-    public void setHotdogCost(float hotdogCost){this.hotdogCost = hotdogCost;}
+    
+    public float getHotdogCost(){
+        return this.hotdogCost;
+    }
+    
+    public void setHotdogCost(float hotdogCost){
+        this.hotdogCost = hotdogCost;
+    }
 
-    public Enum getHotdogMeat(){return this.hotdogMeat;}
-    public void setHotdogMeat(Enum hotdogMeat){this.hotdogMeat = hotdogMeat;}
+    public hotdogMeat getHotdogMeat(){
+        return this.hotdogMeat;
+    }
+    public void setHotdogMeat(hotdogMeat hotdogMeat){
+        this.hotdogMeat = hotdogMeat;
+    }
 
-    public boolean getIsKosher(){return this.isKosher;}
-    public void setIsKosher(boolean isKosher){this.isKosher = isKosher;}
 
-    public int getCalories(){return this.calories;}
-    public void setCalories(int calories){this.calories = calories;}
+    public boolean getIsKosher(){
+        return this.isKosher;
+    }
+    
+    public void setIsKosher(boolean isKosher){
+        this.isKosher = isKosher;
+    }
 
-    public void displayCorndog() {
-        System.out.println("Corndog Report");
-        System.out.println("===========================");
-        System.out.println("Toppings: " + this.toppings);
-        System.out.println("Price: " + this.price);
-        System.out.println("Hotdog Cost: " + this.hotdogCost);
-        System.out.println("Hotdog Meat: " + this.hotdogMeat);
-        System.out.println("Is Kosher: " + this.isKosher);
-        System.out.println("Calories: " + this.calories);
+    public int getCalories(){
+        return this.calories;
+    }
+    
+    public void setCalories(int calories){
+        this.calories = calories;
     }
 
     public String toString() {
@@ -89,8 +113,5 @@ public class Corndog {
         return s;
 
     }
-
-
-
 
 }
