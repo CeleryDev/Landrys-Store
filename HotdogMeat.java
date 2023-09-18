f27f5ee6e8de37860f4d3ea784c414a4c5036abd
Sabrina <33787198+sreese147@users.noreply.github.com>
2023-09-18 15:25:34 -0400

    Update HotdogMeat.java
    comments

--- HotdogMeat.java
+++ HotdogMeat.java
@@ -1,4 +1,4 @@
-/*Sabrina Reese + Ben
+/*Sabrina Reese + Ben Feibus
  * Cmdr Schenk
  * CSA Period 7
  * 19 September 2023
@@ -7,7 +7,9 @@
 
 package reese.teach;
 
+//comments
 public enum HotdogMeat {
+    //meat options
     Beef, 
     Turkey, 
     Pork, 
