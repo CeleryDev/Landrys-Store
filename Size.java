4f63c10c44354218149945c8b9e994ba9e9bdec9
Sabrina <33787198+sreese147@users.noreply.github.com>
2023-09-22 15:22:42 -0400

    Create Size.java

---
+++ Size.java
@@ -0,0 +1,17 @@
+/*Sabrina Reese + Ben Feibus
+ * Cmdr Schenk
+ * CSA Period 7
+ * 25 September 2023
+ * Corndog Box Size
+ */
+
+package reese.teach;
+
+//sabrina
+public enum Size {
+    XS, 
+    S, 
+    M, 
+    L, 
+    XL
+}
